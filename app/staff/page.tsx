21479f4772ee9834be1e1ef055624181e3fae0a6
Kirsten <kirstensutantyo@gmail.com>
2024-10-21 18:02:53 -0400

    changed icon and fixed other pages

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -13,7 +13,7 @@ export default function Overwatch2() {
         {/* Text Overlay */}
         <div className="absolute inset-0 flex items-center justify-center z-10">
           <h1 className="text-9xl max-sm:text-8xl font-bold text-white drop-shadow-2xl">
-            Staff
+            STAFF
           </h1>
         </div>
 
